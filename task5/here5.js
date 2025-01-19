4e90d1f357bd5e285238ae7c8ff2e55f745879f3
nazrinbakirova <nazrinbakirova07@gmail.com>
2025-01-20 02:12:55 +0400

    lab4

---
+++ task5/here5.js
@@ -0,0 +1,41 @@
+// function sumFunc(word,del){
+//     if(word.includes(del)){
+//         console.log(word.slice(0,8));  
+//     }
+//     else {
+//          console.log('');
+//         }
+//         return;
+// }
+
+// sumFunc('This is str','str')
+
+
+// const fullNameConvertor[name,surname] {
+//     return { name.UpperCase[] surname.UpperCase[]}
+// }
+
+
+
+// function fullNameConvertor(name, surname) {
+//    let result = ' ${name} ${surname}';
+//    return result.toUpperCase();
+  
+// }
+
+// console.log(fullNameConvertor("Elgun","Musayev"));
+
+//
+
+// function EmailCheck(email=""){
+//     if(  email.includes("@") && email.endsWith("gmail.com") ){
+//         console.log(email);
+//     }
+//     else{
+//         console.log("not an email");
+//     }
+//         return ;
+// }
+
+// EmailCheck("nazrin@gmail.com");
+
